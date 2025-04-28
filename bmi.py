1ad774981cbd911d929a4e4c92de08aab42f0276
jingzhi <jingzhi.24@ichat.sp.edu.sg>
2025-04-28 11:15:04 +0800

    finish bmi

---
+++ bmi.py
@@ -0,0 +1,21 @@
+def calculate_bmi(height, weight):
+    print("Height="+str(height))
+    print("Weight="+str(weight))
+    bmi = weight/(height*height)
+    return bmi
+
+def main():
+    bmi_value= calculate_bmi(1.65,62)
+    print("The calculated bmi value ="+str(bmi_value))
+    check_bmi(bmi_value)
+
+def check_bmi(bmi_value):
+    if(bmi_value<18.5):
+     print("you are underweight")
+    if(bmi_value>=18.5 and bmi_value<=25.0):
+     print("you have a normal weight") 
+    if(bmi_value>25.0):
+     print("you are overweight")
+
+if __name__ == "__main__":
+   main()
