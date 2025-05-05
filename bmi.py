63a9973e210f877317070ad139d0739b1665af06
jingzhi <jingzhi.24@ichat.sp.edu.sg>
2025-05-05 10:33:49 +0800

    lab3 exercise1

--- bmi.py
+++ bmi.py
@@ -5,17 +5,20 @@ def calculate_bmi(height, weight):
     return bmi
 
 def main():
-    bmi_value= calculate_bmi(1.65,62)
+    bmi_value= calculate_bmi(1.75,62)
     print("The calculated bmi value ="+str(bmi_value))
     check_bmi(bmi_value)
 
 def check_bmi(bmi_value):
     if(bmi_value<18.5):
      print("you are underweight")
+     return -1
     if(bmi_value>=18.5 and bmi_value<=25.0):
      print("you have a normal weight") 
+     return 0
     if(bmi_value>25.0):
      print("you are overweight")
+     return 1
 
 if __name__ == "__main__":
    main()
